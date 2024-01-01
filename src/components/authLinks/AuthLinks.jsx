0768993ffac48af96883b13fceb0c0000aca93aa
João Bastos <japbastos@gmail.com>
2024-01-01 19:18:28 -0300

    feat: add main components

---
+++ src/components/authLinks/AuthLinks.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styles from './authLinks.module.css'
+import Link from 'next/link'
+
+export const AuthLinks = () => {
+  const status = 'notauthenticated'
+  return (
+    <>
+      {status === 'notauthenticated' ? (
+        <Link  href='/login'>Login</Link>
+      ) : (
+        <>
+          <Link  href='/write'>Write</Link>
+          <span className={styles.link}>Logout</span>
+        </>
+      ) }
+    </>
+  )
+}
---
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './themeToggle.module.css'
+
+export const ThemeToggle = () => {
+  return (
+    <div className={styles.container}>
+      ThemeToggle
+    </div>
+  )
+}
